fa9ca517802c3f60330608db0ddaeb0c1f3cc344
Felix Eckhofer <felix@eckhofer.com>
2013-03-27 10:50:51 +0100

    bump version number

--- videoframes/init.php
+++ videoframes/init.php
@@ -2,7 +2,7 @@
 class VideoFrames extends Plugin {
 
 	function about() {
-		return array(0.1,
+		return array(0.2,
 			"Enable the playback of embedded videos from well-known sites",
 			"dxbi",
 			false);
